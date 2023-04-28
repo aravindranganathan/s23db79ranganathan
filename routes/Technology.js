10b83eb5d58f504ca7b968c26cf4ed32cfc9f9ad
aravindranganathan <aravindranganathan007@gmail.com>
2023-04-27 19:34:32 -0500

    After authentication.

--- routes/Technology.js
+++ routes/Technology.js
@@ -5,5 +5,14 @@ var router = express.Router();
 router.get('/', technology_controllers.technology_view_all_Page )
 router.get('/detail', technology_controllers.technology_view_one_Page);
 router.get('/create', technology_controllers.technology_create_Page);
-router.get('/update', technology_controllers.technology_update_Page);
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ req.session.returnTo = req.originalUrl;
+ res.redirect("/login");
+ }
+ router.get('/update',secured, technology_controllers.technology_update_Page);
 module.exports = router;
